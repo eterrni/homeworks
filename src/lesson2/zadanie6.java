7f5f1f2b8524b3a3142c7ee7e3c1c028c332d262
eterrni <61310114+eterrni@users.noreply.github.com>
2020-02-26 13:59:46 +0300

    homework-2

--- src/lesson2/zadanie6.java
+++ src/lesson2/zadanie6.java
@@ -11,14 +11,17 @@ public class zadanie6 {
             scan.next();
         }
         int n = scan.nextInt();
-        if(n<=1){
-            System.out.println(n+" - не является прсстым числом");
-        } else if(n==2 || n==3 || n==5 || n==7){
-            System.out.println(n+" - является простым числом");
-        } else if(n%2==0 || n%3==0|| n%5==0|| n%7==0){
-            System.out.println(n+" - не является простым числом");
-        }else {
-            System.out.println(n+" - является простым числом");
+        int a=0;
+        for (int i=1;i<=n;i++){
+            if (n%i==0){
+                a++;
+            }if(i!=n-1 && a==2){
+                System.out.println(n+" не является простым числом");
+                break;
+            } else if(i==n-1 && a==1){
+                System.out.println(n+" является простым числом");
+                break;
+            }
         }
     }
 }
