9c9504d62b404b1773a1b53a897ddecd65439285
eterrni <61310114+eterrni@users.noreply.github.com>
2020-02-27 12:39:43 +0300

    homework-2

---
+++ src/lesson3/zadanie1.java
@@ -0,0 +1,33 @@
+package lesson3;
+
+import java.util.Scanner;
+
+public class zadanie1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int sec = scan.nextInt();
+        int min = scan.nextInt();
+        int hours = scan.nextInt();
+        Time TimeInterval_1 = new Time(sec, min, hours);
+        Time TimeInterval_2 = new Time(Time.transferToSeconds(sec, min, hours));
+        Time.dataOutput(sec, min, hours);
+    }
+
+
+    static class Time {
+        Time(int seconds) {
+        }
+
+        Time(int sec, int min, int hours) {
+        }
+
+        static int transferToSeconds(int sec, int min, int hours) {
+            return sec + min * 60 + hours * 3600;
+        }
+
+        static void dataOutput(int sec, int min, int hours) {
+            System.out.println(sec + ":" + min + ":" + hours);
+        }
+    }
+}
+
