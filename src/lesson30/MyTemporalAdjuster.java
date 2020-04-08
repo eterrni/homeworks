780e33d4d5ef192abcf8602363a8668efba73c5e
eterrni <61310114+eterrni@users.noreply.github.com>
2020-04-08 17:41:47 +0300

    homework-30 ( Date Api )

--- src/lesson30/MyTemporalAdjuster.java
+++ src/lesson30/MyTemporalAdjuster.java
@@ -1,9 +1,7 @@
 package lesson30;
 
-import java.time.temporal.ChronoUnit;
 import java.time.temporal.TemporalAdjuster;
 
-/*7.Написать свою реализацию интерфейса TemporalAdjuster, которая бы прибавляла к дате 42 дня*/
 public interface MyTemporalAdjuster {
     TemporalAdjuster add42days();
 
