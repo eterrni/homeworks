0f82678ad35d899faa4f783b6188b87a67709bfa
eterrni <61310114+eterrni@users.noreply.github.com>
2020-04-09 08:55:34 +0300

    homework-30

--- src/lesson30/MyTemporalAdjuster.java
+++ src/lesson30/MyTemporalAdjuster.java
@@ -1,9 +1,34 @@
 package lesson30;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.time.temporal.TemporalAdjuster;
 
 public interface MyTemporalAdjuster {
-    TemporalAdjuster add42days();
 
-    TemporalAdjuster changeDate1stJanuary();
+    static TemporalAdjuster add42days(LocalDate today) {
+        return today.plus(42, ChronoUnit.DAYS);
+    }
+
+    static TemporalAdjuster changeDate1stJanuary(LocalDate today) {
+        if (today.getDayOfYear() < 183) {
+            return firstJanOfThisYear(today);
+        } else return firstJanOfNextYear(today);
+    }
+
+    private static TemporalAdjuster firstJanOfThisYear(LocalDate today) {
+        int todayIsDay = today.getDayOfYear();
+
+
+        return today.minus(todayIsDay - 1, ChronoUnit.DAYS);
+    }
+
+    private static TemporalAdjuster firstJanOfNextYear(LocalDate today) {
+        int daysInYear = today.lengthOfYear();
+        int todayIsDay = today.getDayOfYear();
+        int leftDays = daysInYear - todayIsDay;
+
+        return today.plus(leftDays + 1, ChronoUnit.DAYS);
+
+    }
 }
--- src/lesson30/Task_7_8.java
+++ src/lesson30/Task_7_8.java
@@ -1,47 +1,13 @@
 package lesson30;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
-import java.time.temporal.TemporalAdjuster;
 
 /* 7.Написать свою реализацию интерфейса TemporalAdjuster, которая бы прибавляла к дате 42 дня
    8.(*) Написать свою реализацию интерфейса TemporalAdjuster, которая бы изменяла дату на ближайшее (в днях) 1 января.*/
 public class Task_7_8 implements MyTemporalAdjuster {
-    @Override
-    public TemporalAdjuster add42days() {
-        LocalDate today = LocalDate.now();
-        return today.plus(42, ChronoUnit.DAYS);
-    }
-
-    @Override
-    public TemporalAdjuster changeDate1stJanuary() {
-        LocalDate today = LocalDate.now();
-
-        if (today.getDayOfYear() <= 183) {
-            return firstJanOfThisYear(today);
-        } else return firstJanOfNextYear(today);
-    }
-
-    public TemporalAdjuster firstJanOfThisYear(LocalDate today) {
-        int todayIsDay = today.getDayOfYear();
-
-
-        return today.minus(todayIsDay - 1, ChronoUnit.DAYS);
-    }
-
-    public TemporalAdjuster firstJanOfNextYear(LocalDate today) {
-        int daysInYear = today.lengthOfYear();
-        int todayIsDay = today.getDayOfYear();
-        int leftDays = daysInYear - todayIsDay;
-
-        return today.plus(leftDays + 1, ChronoUnit.DAYS);
-
-    }
-
     public static void main(String[] args) {
-        Task_7_8 task_7_8 = new Task_7_8();
-//        System.out.println(task_7_8.add42days());
-        System.out.println(task_7_8.changeDate1stJanuary());
+        LocalDate today = LocalDate.now();
+        System.out.println(MyTemporalAdjuster.add42days(today));
+        System.out.println(MyTemporalAdjuster.changeDate1stJanuary(today));
     }
 }
